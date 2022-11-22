1d0a64c86cb1f0bd65c939d10c5962ef873800e6
Keen1 <ZachKeener12@gmail.com>
2022-11-22 00:47:00 -0500

    Not decrypting correcting, not sure why

--- AESCipher.java
+++ AESCipher.java
@@ -1,56 +1,24 @@
 package aes;
 
-import aes.Key;
-import aes.CipherBlock;
+
 //cipher body
 public class AESCipher {
-	private String plaintext;
-	private String ciphertext;
-	private String key;
-	CipherBlock stateBlock = new CipherBlock();
-	Key k_Store;
-	private int[][] sBox = {
-			{99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 118},
-			{202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192},
-			{183, 253, 147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21},
-			{4, 199, 35, 195, 24, 150, 5, 154, 7, 18, 128, 226, 235, 39, 178, 117},
-			{9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227, 47, 132},
-			{83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207},
-			{208, 239, 170, 251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168},
-			{81, 163, 64, 143, 146, 157, 56, 245, 188, 182, 218, 33, 16, 255, 243, 210},
-			{205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61, 100, 93, 25, 115},
-			{96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219},
-			{224, 50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121},
-			{231, 200, 55, 109, 141, 213, 78, 169, 108, 86, 244, 234, 101, 122, 174, 8},
-			{186, 120, 37, 46, 28, 166, 180, 198, 232, 221, 116, 31, 75, 189, 139, 138},
-			{112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29, 158},
-			{225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223},
-			{140, 161, 137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22}};
-	private int[][] isBox = {
-			{82, 9, 106, 213, 48, 54, 165, 56, 191, 64, 163, 158, 129, 243, 215, 251},
-			{124, 227, 57, 130, 155, 47, 255, 135, 52, 142, 67, 68, 196, 222, 233, 203},
-			{84, 123, 148, 50, 166, 194, 35, 61, 238, 76, 149, 11, 66, 250, 195, 78},
-			{8, 46, 161, 102, 40, 217, 36, 178, 118, 91, 162, 73, 109, 139, 209, 37},
-			{114, 248, 246, 100, 134, 104, 152, 22, 212, 164, 92, 204, 93, 101, 182, 146},
-			{108, 112, 72, 80, 253, 237, 185, 218, 94, 21, 70, 87, 167, 141, 157, 132},
-			{144, 216, 171, 0, 140, 188, 211, 10, 247, 228, 88, 5, 184, 179, 69, 6},
-			{208, 44, 30, 143, 202, 63, 15, 2, 193, 175, 189, 3, 1, 19, 138, 107},
-			{58, 145, 17, 65, 79, 103, 220, 234, 151, 242, 207, 206, 240, 180, 230, 115},
-			{150, 172, 116, 34, 231, 173, 53, 133, 226, 249, 55, 232, 28, 117, 223, 110},
-			{71, 241, 26, 113, 29, 41, 197, 137, 111, 183, 98, 14, 170, 24, 190, 27},
-			{252, 86, 62, 75, 198, 210, 121, 32, 154, 219, 192, 254, 120, 205, 90, 244},
-			{31, 221, 168, 51, 136, 7, 199, 49, 177, 18, 16, 89, 39, 128, 236, 95},
-			{96, 81, 127, 169, 25, 181, 74, 13, 45, 229, 122, 159, 147, 201, 156, 239},
-			{160, 224, 59, 77, 174, 42, 245, 176, 200, 235, 187, 60, 131, 83, 153, 97},
-			{23, 43, 4, 126, 186, 119, 214, 38, 225, 105, 20, 99, 85, 33, 12, 125}};
+	
+	private StringBuilder plaintextBld = new StringBuilder();
+	private StringBuilder ciphertextBld = new StringBuilder();
+	CipherBlock stateBlock;
+	Key k_Store =  new Key("abvdo10alznl>8?m");
+	
 	
 	//no param init
 	AESCipher(){
 		
-		this.plaintext = "testgkvn12388kln";
-		this.ciphertext = "";
-		this.key = "";
-		this.k_Store = new Key("abvdo10alznl>8?m");
+		this.plaintextBld.append("The quick white fox jumped over the lazy brown dog. ");
+		
+		
+	}
+	AESCipher(String plaintext){
+		this.plaintextBld.append(plaintext);
 		
 	}
 	
@@ -58,126 +26,133 @@ public class AESCipher {
 	//builds a key and a cipher block
 	
 	public void encrypt() {
-		
+		//round 0, start a new state and add round key
+		//while there is more plaintext to encrypt
+		while(!this.plaintextBld.isEmpty()) {
+			//get the next 16 or less bytes of the plaintext
+			this.stateBlock = new CipherBlock(this.get_16byte_Or_Less_Plain());
+			//block state is 0 as the plainbytes were just written to the block,
+			//therefore add round key
+			this.stateBlock.add_Round_Key(this.k_Store.get_Key_State(0));
+			//run encryption rounds
+			for(int i = 1; i <= 10; i++) {
+				this.encrypt_State_Round(i);
+			}
+			//write the block to the ciphertext builder
+			this.ciphertextBld.append(this.stateBlock.out_Block());
 		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	public void encrypt_State_Round(int round) {
+			
+			this.stateBlock.sub_Bytes();
+			this.stateBlock.shift_Rows();
+			this.stateBlock.mix_Columns();
+			this.stateBlock.add_Round_Key(this.k_Store.get_Key_State(round));
+	}
+	
+	public void decrypt() {
+		//round 0, start a new state -- keyMap has been generated and indexed, can count down from
+		//keymap dont need to generate key blocks
+		
+		//while there is more ciphertext to decrypt
+		while(!this.ciphertextBld.isEmpty()) {
+			
+			this.stateBlock = new CipherBlock(this.get_16byte_Or_Less_Cipher());
+			//new state block gets round key added first 
+			this.stateBlock.add_Round_Key(this.k_Store.get_Key_State(10));
+			
+			//run decryptioin rounds
+			for(int i = 9; i > -1; i--) {
+				this.decrypt_State_Round(i);
+			}
+			this.plaintextBld.append((this.stateBlock.out_Block()));
+		}
+	}
+	
+	public void decrypt_State_Round(int round) {
+		this.stateBlock.invshift_Rows();
+		this.stateBlock.invsub_Bytes();
+		this.stateBlock.add_Round_Key(this.k_Store.get_Key_State(round));
+		this.stateBlock.invmix_Columns();
+	}
+	
+	public String get_16byte_Or_Less_Plain() {
+		String retStr = "";
+		if(plaintextBld.isEmpty()) {
+			return null;
+		}
+		if(plaintextBld.length() >= 16) {
+			retStr = this.plaintextBld.substring(0, 15);
+			this.plaintextBld.delete(0, 15);
+			return retStr;
+		}
+		else {
+			retStr = this.plaintextBld.toString();
+			this.plaintextBld = new StringBuilder();
+			return retStr;
+		}
+	}
+	
+	public String get_16byte_Or_Less_Cipher() {
+		String retStr = "";
+		if(this.ciphertextBld.isEmpty()) {
+			return null;
+		}
+		if(this.ciphertextBld.length() >= 16) {
+			retStr = this.ciphertextBld.substring(0,15);
+			this.ciphertextBld.delete(0, 15);
+			return retStr;
+		}
+		else {
+			retStr = this.ciphertextBld.toString();
+			this.ciphertextBld = new StringBuilder();
+			return retStr;
+		}
+	}
 	
 	//getters and setters
 	public void set_Plaintext(String plaintext) {
-		this.plaintext = plaintext;
+		this.plaintextBld.append(plaintext);
 	}
 	public String get_Plaintext() {
-		return this.plaintext;
+		return this.plaintextBld.toString();
 	}
 		
 	public void set_Ciphertext(String ciphertext) {
-		this.ciphertext = ciphertext;
+		this.ciphertextBld.append(ciphertext);
 	}
 	public String get_Ciphertext() {
-		return this.ciphertext;
-	}
-		
-	public void set_Key(String key) {
-		this.key = key;
-	}
-	public String get_Key() {
-		return this.key;
+		return this.ciphertextBld.toString();
 	}
 	
 	
 	
-	
-	
-	
-	
-	
-	/*
-	 * test() methods
-	 * Methods intended for testing and verifying results
-	 * */
-	public void test_Cipher_Block(boolean testInput) {
-		
-		if(testInput) {
-			System.out.println("\nCurrent Block State: " + "\nTest input : abcdefghijklmnop" + 
-				"\n___________________________________________");
-		
-			this.stateBlock.test_input("abcdefghijklmnop");
-			this.stateBlock.test_Block();
-			
-		}else {
-			this.stateBlock.test_Block();
+	public void cmd_Menu() {
+		for(int i = 0; i < 2; i++) {
+			System.out.println("*************************************************");
 		}
-		this.stateBlock = new CipherBlock();
+		System.out.println("TESTING ENCRPYTION");
+		System.out.println("*************************************************");
+		System.out.println("Plaintext: " + this.get_Plaintext());
+		System.out.println("Encrypting...");
+		this.encrypt();
+		System.out.println("Complete.");
+		System.out.println("Ciphertext: " + this.get_Ciphertext());
+		System.out.println("*************************************************"
+				+ "\n*************************************************");
+		System.out.println("Decrypting...");
+		this.decrypt();
+		System.out.println("Complete.");
+		System.out.println("Plaintext: " + this.get_Plaintext());
+		
 		
 	}
-	
-	public void test_Mix_Columns() {
-		System.out.println("\n");
-		System.out.println("\nCurrentBlock State: " + "\n___________________________________________");
-		
-		
-		this.stateBlock.test_input("YNJ>?-.&OP@!#KSL");
-		this.stateBlock.test_Block();
-		System.out.println("\n\nPerform mix columns operation");
-		this.stateBlock.mix_Columns();
-		System.out.println("\n");
-		System.out.println("\nCurrentBlock State: " + "\n___________________________________________");
-		this.stateBlock.test_Block();
-		
-	}
-	
-	public void test_Shift_Rows() {
-		this.stateBlock.test_input("123456789ABCDEFG");
-		
-		System.out.println("\nCurrentBlock State: " + "\n___________________________________________");
-		this.stateBlock.test_Block();
-		System.out.println("\nPerform shift rows operations");
-		this.stateBlock.shift_Rows();
-		System.out.println("\nCurrentBlock State: " + "\n___________________________________________");
-		this.stateBlock.test_Block();
-		
-		this.stateBlock = new CipherBlock();
-	}
-	
-	public void test_SBoxes() {
-		System.out.println("S-BOX HEXADECIMAL VALUES");
-		System.out.println("___________________________________________");
-		for(int i = 0; i < this.sBox.length; i++) {
-			System.out.println();
-			for(int k = 0; k < this.sBox[i].length; k++) {
-				String hexS = Integer.toHexString(this.sBox[i][k]);
-				if(hexS.length() == 1) {
-					String hexFull = "0";
-					hexFull += hexS;
-					hexS = hexFull;
-				}
-				hexS = hexS.toUpperCase();
-				System.out.print(hexS);
-				System.out.print(" ");
-			}
-			
-		}
-		System.out.println();
-		System.out.println("___________________________________________");
-		System.out.println();
-		
-		System.out.println("INVERSE-S-BOX HEXADECIMAL VALUES");
-		System.out.println("___________________________________________");
-		
-		for(int i = 0; i < this.isBox.length; i++) {
-			System.out.println();
-			for(int k = 0; k < this.isBox[i].length; k++) {
-				String hexS = Integer.toHexString(this.isBox[i][k]);
-				if(hexS.length() == 1) {
-					String hexFull = "0";
-					hexFull += hexS;
-					hexS = hexFull;
-				}
-				hexS = hexS.toUpperCase();
-				System.out.print(hexS);
-				System.out.print(" ");
-			}
-		}
-		this.stateBlock = new CipherBlock();
-	}
-	
+
 }
--- CipherBlock.java
+++ CipherBlock.java
@@ -3,45 +3,48 @@ package aes;
 public class CipherBlock {
 	
 	//block matrix
-	private int[][] block = new int[4][4];;
-	private int[][] sBox = {
-			{99, 124, 119, 123, 242, 107, 111, 197, 48, 1, 103, 43, 254, 215, 171, 118},
-			{202, 130, 201, 125, 250, 89, 71, 240, 173, 212, 162, 175, 156, 164, 114, 192},
-			{183, 253, 147, 38, 54, 63, 247, 204, 52, 165, 229, 241, 113, 216, 49, 21},
-			{4, 199, 35, 195, 24, 150, 5, 154, 7, 18, 128, 226, 235, 39, 178, 117},
-			{9, 131, 44, 26, 27, 110, 90, 160, 82, 59, 214, 179, 41, 227, 47, 132},
-			{83, 209, 0, 237, 32, 252, 177, 91, 106, 203, 190, 57, 74, 76, 88, 207},
-			{208, 239, 170, 251, 67, 77, 51, 133, 69, 249, 2, 127, 80, 60, 159, 168},
-			{81, 163, 64, 143, 146, 157, 56, 245, 188, 182, 218, 33, 16, 255, 243, 210},
-			{205, 12, 19, 236, 95, 151, 68, 23, 196, 167, 126, 61, 100, 93, 25, 115},
-			{96, 129, 79, 220, 34, 42, 144, 136, 70, 238, 184, 20, 222, 94, 11, 219},
-			{224, 50, 58, 10, 73, 6, 36, 92, 194, 211, 172, 98, 145, 149, 228, 121},
-			{231, 200, 55, 109, 141, 213, 78, 169, 108, 86, 244, 234, 101, 122, 174, 8},
-			{186, 120, 37, 46, 28, 166, 180, 198, 232, 221, 116, 31, 75, 189, 139, 138},
-			{112, 62, 181, 102, 72, 3, 246, 14, 97, 53, 87, 185, 134, 193, 29, 158},
-			{225, 248, 152, 17, 105, 217, 142, 148, 155, 30, 135, 233, 206, 85, 40, 223},
-			{140, 161, 137, 13, 191, 230, 66, 104, 65, 153, 45, 15, 176, 84, 187, 22}};
-	private int[][] isBox = {
-			{82, 9, 106, 213, 48, 54, 165, 56, 191, 64, 163, 158, 129, 243, 215, 251},
-			{124, 227, 57, 130, 155, 47, 255, 135, 52, 142, 67, 68, 196, 222, 233, 203},
-			{84, 123, 148, 50, 166, 194, 35, 61, 238, 76, 149, 11, 66, 250, 195, 78},
-			{8, 46, 161, 102, 40, 217, 36, 178, 118, 91, 162, 73, 109, 139, 209, 37},
-			{114, 248, 246, 100, 134, 104, 152, 22, 212, 164, 92, 204, 93, 101, 182, 146},
-			{108, 112, 72, 80, 253, 237, 185, 218, 94, 21, 70, 87, 167, 141, 157, 132},
-			{144, 216, 171, 0, 140, 188, 211, 10, 247, 228, 88, 5, 184, 179, 69, 6},
-			{208, 44, 30, 143, 202, 63, 15, 2, 193, 175, 189, 3, 1, 19, 138, 107},
-			{58, 145, 17, 65, 79, 103, 220, 234, 151, 242, 207, 206, 240, 180, 230, 115},
-			{150, 172, 116, 34, 231, 173, 53, 133, 226, 249, 55, 232, 28, 117, 223, 110},
-			{71, 241, 26, 113, 29, 41, 197, 137, 111, 183, 98, 14, 170, 24, 190, 27},
-			{252, 86, 62, 75, 198, 210, 121, 32, 154, 219, 192, 254, 120, 205, 90, 244},
-			{31, 221, 168, 51, 136, 7, 199, 49, 177, 18, 16, 89, 39, 128, 236, 95},
-			{96, 81, 127, 169, 25, 181, 74, 13, 45, 229, 122, 159, 147, 201, 156, 239},
-			{160, 224, 59, 77, 174, 42, 245, 176, 200, 235, 187, 60, 131, 83, 153, 97},
-			{23, 43, 4, 126, 186, 119, 214, 38, 225, 105, 20, 99, 85, 33, 12, 125}};
+	private int[][] block = new int[4][4];
 	
+	private SubBoxes sBoxes = new SubBoxes();
 	
 	//cipherBlock constructor, a block is always a 4x4 matrix of bytes
-	CipherBlock(){
+	//a string passed 
+	CipherBlock(String plainBytes){
+		this.write_Block(plainBytes);
+	}
+	
+	private void write_Block(String plainBytes) {
+		
+		
+		int cnt = 0;
+		for(int i = 0; i < this.block.length; i++) {
+			for(int k = 0; k < this.block[i].length; k++) {
+				if(cnt < plainBytes.length()) {
+					this.block[k][i] = plainBytes.charAt(cnt);
+					cnt++;
+				}else {
+					this.block[k][i] = 0;
+				}
+				
+			}
+		}
+	}
+	public String out_Block() {
+		StringBuilder outStr = new StringBuilder();
+		for(int i = 0; i < this.block.length; i++) {
+			for(int k = 3; k > -1; k--) {
+				outStr.append((char)this.block[k][i]);
+			}
+		}
+		return outStr.toString();
+	}
+	
+	public boolean valid_Byte_Str(String plainBytes) {
+		if(plainBytes.length() > 16) {
+			return false;
+		}else {
+			return true;
+		}
 	}
 	
 	/*add_Round_Key() function
@@ -78,12 +81,33 @@ public class CipherBlock {
 		
 		for(int i = 0; i < this.block.length; i++) {
 			scalarRow = matrixFactor[i];
-			for(int k = 0; k < this.block.length; k++) {
+			for(int k = 0; k < this.block[i].length; k++) {
 				int[] stateColumn = {this.block[0][k], this.block[1][k], this.block[2][k], this.block[3][k]};
 				mixedBlock[i][k] = this.matrix_GF_Mult_Op(scalarRow, stateColumn);
 			}
 		}
 		this.block = mixedBlock;
+	}
+	
+	public void invmix_Columns() {
+		int[][] inv_MatrixFactor = {
+				{14,11,13,9},
+				{9,14,11,13},
+				{13,9,14,11},
+				{11, 13, 9, 14}};
+		
+		int[][] mixedBlock = new int[4][4];
+		int[] scalarRow;
+		
+		for(int i = 0; i < this.block.length; i++) {
+			scalarRow = inv_MatrixFactor[i];
+			for(int k = 0; k < this.block[i].length; k++) {
+				int[] stateColumn = {this.block[0][k], this.block[1][k], this.block[2][k], this.block[3][k]};
+				mixedBlock[i][k] = this.matrix_GF_Mult_Op(scalarRow, stateColumn);
+			}
+		}
+		this.block = mixedBlock;
+		
 	}
 	
 	/*
@@ -96,6 +120,18 @@ public class CipherBlock {
 	
 	private int matrix_GF_Mult_Op(int[] scalarRow, int[] stateColumn) {
 		for(int i = 0; i < scalarRow.length; i++) {
+			if(scalarRow[i] == 14) {
+				stateColumn[i] = this.mod_Prime_Poly((stateColumn[i] << 3)^(stateColumn[i] << 2)^(stateColumn[i] << 1));
+			}
+			if(scalarRow[i] == 13) {
+				stateColumn[i] = this.mod_Prime_Poly((stateColumn[i] << 3)^(stateColumn[i] << 2)^stateColumn[i]);
+			}
+			if(scalarRow[i] == 11) {
+				stateColumn[i] = this.mod_Prime_Poly((stateColumn[i] << 3)^(stateColumn[i] << 1)^stateColumn[i]);
+			}
+			if(scalarRow[i] == 9) {
+				stateColumn[i] = this.mod_Prime_Poly((stateColumn[i] << 3)^stateColumn[i]);
+			}
 			if(scalarRow[i] == 3) {
 				int temp = stateColumn[i];
 				stateColumn[i] = this.mod_Prime_Poly(stateColumn[i] << 1);
@@ -107,6 +143,7 @@ public class CipherBlock {
 		}
 		return this.mod_Prime_Poly((stateColumn[0]^stateColumn[1]^stateColumn[2]^stateColumn[3]));
 	}
+	
 	//every operation is computed over a GF(2^8) where m(x) = 283
 	private int mod_Prime_Poly(int value) {
 		if(value >= 256) {
@@ -171,6 +208,57 @@ public class CipherBlock {
 		
 	}
 	
+	public void invshift_Rows() {
+		//leave first row alone
+		
+		/*
+		 * second row inverse shift
+		 * one-byte right circular shift
+		 * */
+		//get last byte of second row
+		int temp = this.get_Byte(1, 3);
+		//move 3rd element to 4th position
+		this.set_Byte(this.get_Byte(1, 2), 1, 3);
+		//move 2nd element to 3rd position
+		this.set_Byte(this.get_Byte(1, 1), 1, 2);
+		//move 1st element to 2nd position
+		this.set_Byte(this.get_Byte(1, 0), 1, 1);
+		//move 4th element to 1st position
+		this.set_Byte(temp, 1, 0);
+		
+		/*
+		 * third row shift
+		 * two-byte right circular shift
+		 * is functionally equivalent is two-byte left shift on 4 bytes
+		 * */
+		//get temp copies of two right vars
+		temp = this.get_Byte(2, 2);
+		int temp2 = this.get_Byte(2, 3);
+		//shift first two elements right 2
+		this.set_Byte(this.get_Byte(2, 1), 2, 3);
+		this.set_Byte(this.get_Byte(2, 0), 2, 2);
+		//set two temp copies according to 2 byte circ shift
+		this.set_Byte(temp, 2, 0);
+		this.set_Byte(temp2, 2, 1);
+		
+		/*
+		 * fourth row shift
+		 * three-byte right circular shift OR one-byte left circular shift
+		 * */
+		//get first element
+		temp = this.get_Byte(3,0);
+		//shift 2nd element to 1st position
+		this.set_Byte(this.get_Byte(3, 1), 3, 0);
+		//shift 3rd element to 2nd position
+		this.set_Byte(this.get_Byte(3, 2), 3, 1);
+		//shift 4th element to 3rd position
+		this.set_Byte(this.get_Byte(3, 3), 3, 2);
+		//set 1st element to 4th positioin
+		this.set_Byte(temp, 3, 3);
+		
+	
+	}
+	
 	public void sub_Bytes() {
 		StringBuilder sb = new StringBuilder("0");
 		
@@ -190,7 +278,30 @@ public class CipherBlock {
 				String column = String.valueOf(hexStr.charAt(1));
 				int matrixCol = Integer.parseInt(column, 16);
 				
-				this.block[i][k] = this.sBox[matrixRow][matrixCol];
+				this.block[i][k] = this.sBoxes.get_S_Byte(matrixRow, matrixCol);
+			}
+		}
+	}
+	public void invsub_Bytes() {
+		StringBuilder sb = new StringBuilder("0");
+		
+		for(int i = 0; i < this.block.length; i++) {
+			for(int k = 0; k < this.block[i].length; k++) {
+				String hexStr = Integer.toHexString(this.block[i][k]);
+				
+				if(hexStr.length() < 2 ) {
+					sb.append(hexStr);
+					hexStr = sb.toString();
+					sb = new StringBuilder("0");
+				}
+				String row = String.valueOf(hexStr.charAt(0));
+				int matrixRow = Integer.parseInt(row, 16);
+				
+				String column = String.valueOf(hexStr.charAt(1));
+				int matrixCol = Integer.parseInt(column, 16);
+				
+				this.block[i][k] = this.sBoxes.get_IS_Byte(matrixRow, matrixCol);
+				
 			}
 		}
 	}
@@ -203,6 +314,7 @@ public class CipherBlock {
 	private void set_Byte(int newByte, int row, int column) {
 		this.block[row][column] = newByte;
 	}
+	
 	
 	
 	/*
@@ -239,5 +351,6 @@ public class CipherBlock {
 				System.out.print(" ");
 			}
 		}
+		System.out.println();
 	}
 }
